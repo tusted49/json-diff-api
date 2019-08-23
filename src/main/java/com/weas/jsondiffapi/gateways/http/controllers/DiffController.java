9251cbbc9f48b1d13b8c0ad46985a51ad68d7be8
Eduardo <eduardo.gaudino@gmail.com>
2019-08-23 02:43:13 -0300

    Add swagger

--- src/main/java/com/weas/jsondiffapi/gateways/http/controllers/DiffController.java
+++ src/main/java/com/weas/jsondiffapi/gateways/http/controllers/DiffController.java
@@ -1,11 +1,14 @@
 package com.weas.jsondiffapi.gateways.http.controllers;
 
+import static org.springframework.http.HttpStatus.OK;
+
 import com.weas.jsondiffapi.domains.Data;
 import com.weas.jsondiffapi.exceptions.NotFoundException;
 import com.weas.jsondiffapi.gateways.http.resources.request.DataRequest;
 import com.weas.jsondiffapi.gateways.http.resources.response.DataResponse;
 import com.weas.jsondiffapi.usecases.CreateData;
 import com.weas.jsondiffapi.usecases.DiffData;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +27,7 @@ public class DiffController {
   private final CreateData createData;
   private final DiffData diffData;
 
+  @ApiOperation(value = "Add Left content")
   @PostMapping("/{id}/left")
   public ResponseEntity addLeft(@PathVariable final Long id,
       @RequestBody final DataRequest dataRequest) {
@@ -33,6 +38,7 @@ public class DiffController {
         .build()));
   }
 
+  @ApiOperation(value = "Add right content")
   @PostMapping("/{id}/right")
   public ResponseEntity addRight(@PathVariable final Long id,
       @RequestBody final DataRequest dataRequest) {
@@ -43,7 +49,9 @@ public class DiffController {
         .build()));
   }
 
+  @ApiOperation(value = "Evaluate the difference between contents")
   @GetMapping("/{id}")
+  @ResponseStatus(OK)
   public DataResponse diff(@PathVariable final Long id) throws NotFoundException {
     return diffData.diff(id);
   }
